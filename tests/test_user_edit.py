a8b90e7198b1a827c5a0ea3d1f616f9a990a6573
vsumbaev <vsumbaev@s-terra.com>
2025-01-13 22:48:19 +0300

    Ex17

--- tests/test_user_edit.py
+++ tests/test_user_edit.py
@@ -1,6 +1,9 @@
 import requests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
+import random
+import string
+
 
 class TestUserEdit(BaseCase, Assertions):
     def test_edit_just_created_user(self):
@@ -70,14 +73,13 @@ class TestUserEdit(BaseCase, Assertions):
 
         user_id = self.get_json_value(response_register, "id")
 
-        print(user_id)
-
         # Edit
         new_name = "Changed_name"
 
         response_edit = requests.put(f"https://playground.learnqa.ru/api/user/{user_id}",
                                      data={"firstName":new_name})
         
+        Assertions.assert_code_status(response_edit, 400)
         Assertions.assert_response_content(response_edit, '{"error":"Auth token not supplied"}')
 
     
@@ -128,5 +130,99 @@ class TestUserEdit(BaseCase, Assertions):
                                      cookies={"auth_sid":auth_sid},
                                      data={"firstName":new_name})
         
+
+        Assertions.assert_code_status(response_edit, 400)
         Assertions.assert_response_content(response_edit, '{"error":"This user can only edit their own data."}')
 
+
+    def test_edit_email_just_created_user_ex17(self):
+
+        # Register
+        register_data = self.prepare_registration_data()
+
+        response_register = requests.post("https://playground.learnqa.ru/api/user/",
+                                  data=register_data)
+        
+        Assertions.assert_code_status(response_register, 200)
+        Assertions.assert_json_has_key(response_register, "id")
+
+        email = register_data['email']
+
+        #first_name = register_data['firstName']
+
+        #last_name = register_data['lastName']
+
+        password = register_data['password']
+
+        user_id = self.get_json_value(response_register, "id")
+
+        # Login 
+        login_data = {
+
+            'email': email,
+            'password': password
+        }
+
+        response_login = requests.post("https://playground.learnqa.ru/api/user/login",
+                                  data=login_data)
+        
+        auth_sid = self.get_cookie(response_login, "auth_sid")
+
+        token = self.get_header(response_login, "x-csrf-token")
+
+
+        # Edit
+        new_email = "vsumbaev-example.com"
+
+        response_edit = requests.put(f"https://playground.learnqa.ru/api/user/{user_id}", 
+                                     headers={"x-csrf-token":token},
+                                     cookies={"auth_sid":auth_sid},
+                                     data={"email":new_email})
+        
+        Assertions.assert_code_status(response_edit, 400)
+        Assertions.assert_response_content(response_edit, '{"error":"Invalid email format"}')
+
+
+    def test_edit_short_fisrt_name_just_created_user_ex17(self):
+
+        # Register
+        register_data = self.prepare_registration_data()
+
+        response_register = requests.post("https://playground.learnqa.ru/api/user/",
+                                  data=register_data)
+        
+        Assertions.assert_code_status(response_register, 200)
+        Assertions.assert_json_has_key(response_register, "id")
+
+        email = register_data['email']
+
+        password = register_data['password']
+
+        user_id = self.get_json_value(response_register, "id")
+
+        # Login 
+        login_data = {
+
+            'email': email,
+            'password': password
+        }
+
+        response_login = requests.post("https://playground.learnqa.ru/api/user/login",
+                                  data=login_data)
+        
+        auth_sid = self.get_cookie(response_login, "auth_sid")
+
+        token = self.get_header(response_login, "x-csrf-token")
+
+
+        # Edit
+        random_symbol_new_name = ''.join(random.choices(string.ascii_letters + string.digits, k=1))
+
+        response_edit = requests.put(f"https://playground.learnqa.ru/api/user/{user_id}", 
+                                     headers={"x-csrf-token":token},
+                                     cookies={"auth_sid":auth_sid},
+                                     data={"firstName":random_symbol_new_name})
+        
+        Assertions.assert_code_status(response_edit, 400)
+
+        Assertions.assert_response_content(response_edit, '{"error":"The value for field `firstName` is too short"}')
